598994b68b8496f7b1fc4ebca60ded12fc99e9c5
bbanks2 <brad.banks@gmail.com>
2021-12-14 12:41:29 -1000

    An in-progress check in. Works for A and B, with a recursive algo for B, but not efficient enough to handle 40 steps.

--- AOC2021/p14.py
+++ AOC2021/p14.py
@@ -15,49 +15,71 @@ class PolymerPairInserter():
     def __init__(self):
         self.rules = {}
         self.start = {}
-        self.freqcnts = [0] * 26
+        self.callstack = 0
+        self.totalrecursecalls = 0
+        self.maxcallstack = 0
 
-    def readRules(w):
+    def setRules(self, rules):
+        self.rules = rules.copy()
+
+    def setStart(self, start):
+        self.start = start
+
+    def recursePair(self, pair, numsteps):
+        if(numsteps == 0):
+            self.freqcnt[ord(pair[0]) - ord('A')] += 1
+            # print("Recpair reached base for end pair: {0} , now self.freqcnt is: {1}".format(pair, self.freqcnt))
+            return
+        inschar = self.rules.get(pair)
+        npair1 = pair[0] + inschar
+        npair2 = inschar + pair[1]
+        self.totalrecursecalls += 1
+        if (self.totalrecursecalls % 1000000 == 0):
+            print("At totalcalls: {0} numsteps: {1} for pair: {2} Callstack: {3} Freqcnt: {4}".format(
+                self.totalrecursecalls, numsteps, pair, self.callstack, self.freqcnt))
+        self.callstack += 1
+        if (self.callstack > self.maxcallstack):
+            self.maxcallstack = self.callstack
+        self.recursePair(npair1, numsteps - 1)
+        self.recursePair(npair2, numsteps - 1)
+        self.callstack -= 1
+#        print("With numsteps: ", numsteps, " left, nstr is: ", nstr)
+        return
+
+    def getFreqcnt(self, numsteps):
+        self.freqcnt = [0] * 26
+        for j in range(1, len(self.start)):
+            pstr = self.start[j - 1:j + 1]
+            print("Starting recursion for start pair #: {0} pair: {1} for numsteps: {2}".format(j, pstr, numsteps))
+            self.recursePair(pstr, numsteps)
+#            print("pstr: {0} numsteps: {1} Nstr: {2}".format(pstr, numsteps, nstr))
+#            dupl_adjust = 1
+#            if(j == len(self.start) - 1):
+#                dupl_adjust = 0
+#            for i in range(len(nstr) - dupl_adjust):
+#                freqcnt[ord(nstr[i]) - ord('A')] += 1
+        self.freqcnt[ord(self.start[j]) - ord('A')] += 1
+        print("Sum freqcnt: {0} Maxcallstack: {1} Freqcnt: {2}".format(sum(self.freqcnt), self.maxcallstack, self.freqcnt))
+        return self.freqcnt
 
 def ansa():
     return 0
 
-def recurse_help(rules, start, numsteps):
-
-
 def ansb(rules, start, numsteps):
-    nstrs = []
-    for j in range(1, len(start)):
-        pstr = start[j-1:j+1]
-        nstrs.append('')
-        for i in range(numsteps):
-            nstr = ''
-            for k in range(1, len(pstr)):
-                inschar = rules.get(pstr[k-1:k+1])
-                nstr = nstr + pstr[k-1] + inschar
-#            print(nstr)
-            pstr = nstr + pstr[-1]
-            if(len(pstr) > 32 and i < numsteps - 1):
-                fcnt1 = recurse_help(rules, pstr(:22), numsteps-i)
-                fcnt2 = recurse_help(rules, pstr[22:], numsteps-i)
-        if(j < len(start) - 1):
-            nstrs[j-1] = pstr[:-1]
-        else:
-            nstrs[j-1] = pstr
-#        print("After step #: ", i, " nstrs[", j-1, "] is: ", nstrs[j-1])
+    ppi = PolymerPairInserter()
+    ppi.setRules(rules)
+    ppi.setStart(start)
+    freqcnt = ppi.getFreqcnt(numsteps)
 
-
-    freqcnt = [0] * 26
-    for s in nstrs:
-        for i in range(len(s)):
-            freqcnt[ord(s[i]) - ord('A')] += 1
-    print("Freqcnt: ", freqcnt)
-    while ( 0 in freqcnt):
+    while (0 in freqcnt):
         freqcnt.remove(0)
-    return max(freqcnt) - min(freqcnt)
+
+    return(max(freqcnt) - min(freqcnt))
 
 
 def main():
+    numstepsa = 10
+    numstepsb = 10
     f = open(fname)
     lines = f.readlines()
     i = 0
@@ -77,13 +99,12 @@ def main():
     print(rules)
 
     pstr = start
-    numsteps = 10
     nstr = ""
-    for i in range(numsteps):
+    for i in range(numstepsa):
         nstr = ""
         for j in range(1,len(pstr)):
 
-#            print(pstr[j-1:j+1])
+            #            print(pstr[j-1:j+1])
             inschar = rules.get(pstr[j-1:j+1])
             nstr = nstr + pstr[j-1] + inschar
 #            print(nstr)
@@ -94,12 +115,12 @@ def main():
     for i in range(len(pstr)):
         freqcnt[ord(pstr[i]) - ord('A')] += 1
     print("Freqcnt: ", freqcnt)
-    while ( 0 in freqcnt):
+    while (0 in freqcnt):
         freqcnt.remove(0)
 
     resa = max(freqcnt) - min(freqcnt)
     print("Resa: {0}".format(resa))
-    resb = ansb(rules, start, 20)
+    resb = ansb(rules, start, numstepsb)
     print("Resb: {0}".format(resb))
 
 main()
